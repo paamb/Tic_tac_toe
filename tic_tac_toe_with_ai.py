009e804441fbe48ee9d53a2e1ec532882d493085
Paal Markus Bjørnstad <paamb@stud.ntnu.no>
2020-08-21 17:57:00 +0200

    Able to choose starting player

--- tic_tac_toe_with_ai.py
+++ tic_tac_toe_with_ai.py
@@ -3,9 +3,20 @@ import math
 board = [['_','_','_'],['_','_','_'],['_','_','_']]
 rewards = [1,0,-1]
 move_counter = 0
+winning_boards = 0
+losing_boards = 0
+draw_boards = 0
 
 ai = "X"
 player = "O"
+def start(move_counter):
+    starting_player = input("Do you want to start (y/n)?")
+    if starting_player == "y":
+        move_counter = 0
+    else:
+        move_counter = 1
+    play(move_counter)
+
 def print_board(board):
         for i in range (len(board)):
             if i != 0:
@@ -18,7 +29,6 @@ def print_board(board):
 def make_a_move(row, col, player, move_counter):
     row = int(row)
     col = int(col)
-    # print(player)
     if board[row][col] == '_':
         board[row][col] = player
         move_counter += 1
@@ -28,10 +38,8 @@ def make_a_move(row, col, player, move_counter):
 
 def mini_max_scoring(board, player):
     #Checking horisontals
-    # print(player)
     for i in range(len(board)):
         if board[i][0] == board[i][1] == board[i][2] == player:
-            # print(print_board(board))
             if player == "X":
                 return 10
             if player == "O":
@@ -39,7 +47,6 @@ def mini_max_scoring(board, player):
     #Checking verticals
     for i in range (len(board)):
         if board[0][i] == board[1][i] == board[2][i] == player:
-            # print(print_board(board))
             if player == "X":
                 return 10
             if player == "O":
@@ -80,21 +87,29 @@ def game_end(board, player):
 def play(move_counter):
     print_board(board)
     if move_counter%2:
-        print("\n\n")
+        print("\n\n\n")
         row, col= input("Skriv inn trekk: ").split()
         player = 'O'
         move_counter = make_a_move(row, col, player, move_counter)
     else:
-        print("\n\n")
+        print("\n\n\n")
         row, col = best_ai_move(board)
-        player = 'X'
-        move_counter = make_a_move(row, col, player, move_counter)
+        player = "X"
+        board[row][col] = player 
+        move_counter += 1
     if game_end(board, player) == False:
         play(move_counter)
-    # else:
-    #     print("GAME OVER")
+    else:
+        print_board(board)
+        print("\n")
+        print("GAME OVER")
+
 def best_ai_move(board):
     best_move_score = -math.inf
+    losing_boards = int(0)
+    winning_boards = int(0)
+    draw_boards = int(0)
+    different_outcomes = [winning_boards, draw_boards, losing_boards]
     best_move_x = -1
     best_move_y = -1
     for i in range(3):
@@ -102,19 +117,29 @@ def best_ai_move(board):
             if board[i][j] == "_":
                 board[i][j] = "X"
                 player = "X"
-                value_of_move = mini_max(board, 0, False, player)
-                # print(value_of_move)
+                value_of_move = mini_max(board, 0, False, player, different_outcomes)                    
+                board[i][j] = "_"
                 if best_move_score < value_of_move:
-                    print(best_move_x, best_move_y)
                     best_move_score = value_of_move
                     best_move_x = i
                     best_move_y = j
-                board[i][j] = "_"
+    if best_move_score == 10:
+        print("AI says: 'Imagine playing this bad'")
+    elif best_move_score == 0:
+        print("AI says: 'Okay good move sir'")
+    elif best_ai_move == -10:
+        print("AI says: 'Do you cheat?'")
     return (best_move_x, best_move_y)
 
-def mini_max(pos, depth, maximizing_player, player):
+def mini_max(pos, depth, maximizing_player, player, different_outcomes):
     score = mini_max_scoring(board, player)
     if score is not None:
+        if score == 10:
+            different_outcomes[0] += 1
+        if score == -10:
+            different_outcomes[2] += 1
+        if score == 0:
+            different_outcomes[1] += 1
         return score
     if maximizing_player:
         best_val = -(math.inf)
@@ -123,7 +148,7 @@ def mini_max(pos, depth, maximizing_player, player):
                 if board[i][j] == "_":
                     player = "X"
                     board[i][j] = "X"
-                    value = mini_max(board, depth +1, False, player)
+                    value = mini_max(board, depth +1, False, player, different_outcomes)
                     board[i][j] = "_"
                     best_val = max(best_val, value)
         return best_val
@@ -134,9 +159,12 @@ def mini_max(pos, depth, maximizing_player, player):
                 if board[i][j] == "_":
                     player = "O"
                     board[i][j] = "O"
-                    value = mini_max(board, depth +1, True, player)
+                    value = mini_max(board, depth +1, True, player, different_outcomes)
                     board[i][j] = "_"
                     best_val = min(best_val, value)
-        return best_val                                                                                                           
+        return best_val                                                                                                        
 
-play(move_counter)
+start(move_counter)
+
+
+#Viser for den over. Og viser for alle verdier og ikke de verdiene som er de beste
